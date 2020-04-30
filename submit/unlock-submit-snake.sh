a4c07ea71d4868b69e214c541f3d373c59b63d1d
akrinos <akrinos@vt.edu>
2020-04-30 09:58:54 -0400

    Changing busco arrangement

---
+++ submit/unlock-submit-snake.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --qos=unlim
+#SBATCH --mem=100gb
+
+snakemake   \
+    --unlock --jobs 100 --rerun-incomplete --use-conda --rerun-incomplete --cluster-config cluster.yaml --cluster "sbatch --parsable --qos=unlim --partition={cluster.queue} --job-name=batseukrhythmic.{rule}.{wildcards} --mem={cluster.mem}gb --time={cluster.time} --ntasks={cluster.threads} --nodes={cluster.nodes}"
