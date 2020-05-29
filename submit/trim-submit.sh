de07ad4bcc7a517d1b9238b5afe0b74a1851fcdd
akrinos <akrinos@vt.edu>
2020-05-29 08:20:32 -0400

    Removing files specific to particular projects

--- submit/trim-submit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-snakemake   \
-        --jobs 100 --use-conda -s ../modules/trimmomatic-snake  \
-        --cluster-config ../cluster.yaml --cluster "sbatch --parsable --qos=unlim --partition={cluster.queue} --job-name=WAP.{rule}.{wildcards} --mem={cluster.mem}gb --time={cluster.time} --ntasks={cluster.threads} --nodes={cluster.nodes}"
-
