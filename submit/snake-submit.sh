80466bc9a076cbb91eb95d9218ef7aaa3bf58fa4
akrinos <akrinos@vt.edu>
2020-06-01 07:15:03 -0400

    Fixing another typo in transabyss rule and commenting out new interactive script

--- submit/snake-submit.sh
+++ submit/snake-submit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --qos=unlim
 #SBATCH --time=5000
-#SBATCH --partition=scavenger
+#SBATCH --partition=compute
 #SBATCH --mem=100gb
 
 jobname=$(cat config.yaml | grep jobname | cut -d ":" -f 2 | cut -d " " -f 2)
 outdir=$(cat config.yaml | grep outputDIR | cut -d ":" -f 2 | cut -d " " -f 2)
 
-python scripts/writeconfig.py
+#python scripts/writeconfig.py
 
 snakemake  \
     --rerun-incomplete --jobs 100 --use-conda --cluster-config cluster.yaml --cluster "sbatch --parsable --qos=unlim --partition={cluster.queue} --job-name=${jobname}.{rule}.{wildcards} --mem={cluster.mem}gb --time={cluster.time} --ntasks={cluster.threads} --nodes={cluster.nodes}"
