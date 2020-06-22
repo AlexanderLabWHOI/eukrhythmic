0caa1090f76e6b2675d44c7c23587317df0d6294
akrinos <akrinos@vt.edu>
2020-06-22 09:42:19 -0400

    Fixing Trinity rule to deal with environment variable; need to add flag in config for whether to set this env variable to scratch

--- scripts/autogenerate_metaT_sample.py
+++ scripts/autogenerate_metaT_sample.py
@@ -17,7 +17,7 @@ if (len(inputargs) < 2):
            as an argument. You may also provide the extension as a second argument;\
            the default is .fastq.gz. You can provide a special indication of suffix\
            or forward/reverse reads as a third and fourth argument. The default is _1/_2, or\
-           the full name if no specification is made. A fifth argument may indicate and additional\
+           the full name if no specification is made. A fifth argument may indicate an additional\
            suffix, such as 001.")
     sys.exit()
 else:
@@ -35,6 +35,7 @@ if len(inputargs) > 2:
     extension = inputargs[2]
 # filter directory by files containing proper extension.
 sequencefiles = [curr for curr in candidatefiles if extension in curr]
+print([curr.split("_")[0] for curr in sequencefiles])
 
 ## SPECIFY FORWARD AND REVERSE SPECIFICATION ##
 forwardspec = "_1"
@@ -52,18 +53,24 @@ if len(inputargs) > 5:
 fastqfiles_forward = set([curr.split(forwardspec)[0] for curr in sequencefiles if forwardspec in curr])
 fastqfiles_reverse = set([curr.split(reversespec)[0] for curr in sequencefiles if reversespec in curr])
 fastqfiles_single = set([curr.split(singlespec)[0] for curr in sequencefiles if (forwardspec not in curr) & (reversespec not in curr)])
-    
+
 ## COMBINE ALL FILENAMES ##
 fastalist = list(fastqfiles_forward.intersection(fastqfiles_reverse)) + list(fastqfiles_single)
+print(fastalist)
 
 ## ATTEMPT TO MAKE SHORTER SAMPLE ID NAME ##
 samplelist = [curr.split("_")[0] for curr in fastalist] # try taking first token from name
 if len(set(samplelist)) != len(set(fastalist)):
-    samplelist = fastalist # otherwise just set sample list to fasta names
+    for s in range(len(samplelist)):
+        if samplelist.count(samplelist[s]) > 1:
+            samplelist[s] = str(samplelist[s]) + "_" + str(samplelist.count(samplelist[s]))
+    if len(set(samplelist)) != len(set(fastalist)):        
+        samplelist = fastalist # otherwise just set sample list to fasta names
     
 metaT = pd.DataFrame({"SampleName": fastalist, \
               "SampleID": samplelist, \
               "AssemblyGroup": samplelist, \
               "FastqFile": fastalist,})
+metaT = metaT.sort_values(by="SampleName")
 
 metaT.to_csv(path_or_buf = outfilename, index=False, sep = "\t")
